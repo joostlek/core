d4a4094b6f7e3a3eec0b0aa903511f09f5835fa6
Joostlek <joostlek@outlook.com>
2025-03-16 15:35:36 +0100

    Deprecate Valve binary sensor in SmartThings

--- homeassistant/components/smartthings/binary_sensor.py
+++ homeassistant/components/smartthings/binary_sensor.py
@@ -6,17 +6,26 @@ from dataclasses import dataclass
 
 from pysmartthings import Attribute, Capability, SmartThings
 
+from homeassistant.components.automation import automations_with_entity
 from homeassistant.components.binary_sensor import (
+    DOMAIN as BINARY_SENSOR_DOMAIN,
     BinarySensorDeviceClass,
     BinarySensorEntity,
     BinarySensorEntityDescription,
 )
+from homeassistant.components.script import scripts_with_entity
 from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+from homeassistant.helpers.issue_registry import (
+    IssueSeverity,
+    async_create_issue,
+    async_delete_issue,
+)
 
 from . import FullDevice, SmartThingsConfigEntry
-from .const import MAIN
+from .const import DOMAIN, MAIN
 from .entity import SmartThingsEntity
 
 
@@ -88,6 +97,7 @@ CAPABILITY_TO_SENSORS: dict[
             translation_key="valve",
             device_class=BinarySensorDeviceClass.OPENING,
             is_on_key="open",
+            entity_registry_enabled_default=False,
         )
     },
     Capability.WATER_SENSOR: {
@@ -100,6 +110,13 @@ CAPABILITY_TO_SENSORS: dict[
 }
 
 
+def entity_used_in(hass: HomeAssistant, entity_id: str) -> list[str]:
+    """Get list of related automations and scripts."""
+    used_in = automations_with_entity(hass, entity_id)
+    used_in += scripts_with_entity(hass, entity_id)
+    return used_in
+
+
 async def async_setup_entry(
     hass: HomeAssistant,
     entry: SmartThingsConfigEntry,
@@ -107,20 +124,77 @@ async def async_setup_entry(
 ) -> None:
     """Add binary sensors for a config entry."""
     entry_data = entry.runtime_data
-    async_add_entities(
-        SmartThingsBinarySensor(
-            entry_data.client,
-            device,
-            description,
-            entry_data.rooms,
-            capability,
-            attribute,
-        )
-        for device in entry_data.devices.values()
-        for capability, attribute_map in CAPABILITY_TO_SENSORS.items()
-        if capability in device.status[MAIN]
-        for attribute, description in attribute_map.items()
-    )
+    entity_registry = er.async_get(hass)
+    entities: list[SmartThingsBinarySensor] = []
+
+    def add_deprecated_entity(
+        dev: FullDevice,
+        desc: SmartThingsBinarySensorEntityDescription,
+        cap: Capability,
+        attr: Attribute,
+    ) -> None:
+        """Add deprecated entities."""
+        entity_uid = f"{dev.device.device_id}.{attr}"
+        if entity_id := entity_registry.async_get_entity_id(
+            BINARY_SENSOR_DOMAIN, DOMAIN, entity_uid
+        ):
+            entity_entry = entity_registry.async_get(entity_id)
+            if entity_entry and entity_entry.disabled:
+                entity_registry.async_remove(entity_id)
+                async_delete_issue(
+                    hass,
+                    DOMAIN,
+                    f"deprecated_entity_{entity_uid}",
+                )
+            elif entity_entry:
+                entities.append(
+                    SmartThingsBinarySensor(
+                        entry_data.client,
+                        dev,
+                        desc,
+                        entry_data.rooms,
+                        cap,
+                        attr,
+                    )
+                )
+                if entity_used_in(hass, entity_id):
+                    async_create_issue(
+                        hass,
+                        DOMAIN,
+                        f"deprecated_entity_{entity_uid}",
+                        breaks_in_ha_version="2025.10.0",
+                        is_fixable=False,
+                        severity=IssueSeverity.WARNING,
+                        translation_key="deprecated_entity",
+                        translation_placeholders={
+                            "name": str(
+                                entity_entry.name or entity_entry.original_name
+                            ),
+                            "entity": entity_id,
+                        },
+                    )
+
+    for device in entry_data.devices.values():
+        for capability, attribute_map in CAPABILITY_TO_SENSORS.items():
+            if capability in device.status[MAIN]:
+                if capability is Capability.VALVE:
+                    for attribute, description in attribute_map.items():
+                        add_deprecated_entity(
+                            device, description, capability, attribute
+                        )
+                else:
+                    entities.extend(
+                        SmartThingsBinarySensor(
+                            entry_data.client,
+                            device,
+                            description,
+                            entry_data.rooms,
+                            capability,
+                            attribute,
+                        )
+                        for attribute, description in attribute_map.items()
+                    )
+    async_add_entities(entities)
 
 
 class SmartThingsBinarySensor(SmartThingsEntity, BinarySensorEntity):
--- tests/components/smartthings/test_binary_sensor.py
+++ tests/components/smartthings/test_binary_sensor.py
@@ -6,9 +6,11 @@ from pysmartthings import Attribute, Capability
 import pytest
 from syrupy import SnapshotAssertion
 
+from homeassistant.components.binary_sensor import DOMAIN as BINARY_SENSOR_DOMAIN
+from homeassistant.components.smartthings import DOMAIN
 from homeassistant.const import STATE_OFF, STATE_ON, Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import entity_registry as er
+from homeassistant.helpers import entity_registry as er, issue_registry as ir
 
 from . import setup_integration, snapshot_smartthings_entities, trigger_update
 
@@ -51,3 +53,27 @@ async def test_state_update(
     )
 
     assert hass.states.get("binary_sensor.refrigerator_door").state == STATE_ON
+
+
+@pytest.mark.parametrize("device_fixture", ["virtual_valve"])
+@pytest.mark.usefixtures("entity_registry_enabled_by_default")
+async def test_deprecated_entity(
+    hass: HomeAssistant,
+    devices: AsyncMock,
+    mock_config_entry: MockConfigEntry,
+    issue_registry: ir.IssueRegistry,
+    entity_registry: er.EntityRegistry,
+) -> None:
+    """Test state update."""
+    entity_registry.async_get_or_create(
+        BINARY_SENSOR_DOMAIN,
+        DOMAIN,
+        "612ab3c2-3bb0-48f7-b2c0-15b169cb2fc3.valve",
+        suggested_object_id="volvo_valve",
+    )
+    await setup_integration(hass, mock_config_entry)
+
+    assert hass.states.get("binary_sensor.volvo_valve").state == STATE_OFF
+    assert issue_registry.async_get_issue(
+        DOMAIN, "deprecated_entity_612ab3c2-3bb0-48f7-b2c0-15b169cb2fc3.valve"
+    )
